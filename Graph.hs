4cd619ef3f2a115a65c6f87c557a275cf0f743af
Brett Schreiber <schreiberbrett@gmail.com>
2022-01-27 20:24:27 -0500

    Update Graph.hs

--- Graph.hs
+++ Graph.hs
@@ -4,13 +4,19 @@ import Data.Map (Map)
 import qualified Data.Set as Set
 import Data.Set (Set)
 
-import Data.List (sort)
+import Data.List (sort, intercalate)
 
 import Debug.Trace (trace)
+
+(\/) :: (Ord a) => Set a -> Set a -> Set a
+(\/) = Set.union
 
 neighbors :: (Ord a) => Graph a -> Map a (Set a)
 neighbors g = Map.fromSet (\v ->
     Set.filter (hasEdge g v) (vertices g)) (vertices g)
+
+oddCycleGates :: (Ord a) => Graph a -> Set (Gate (BWGraph a))
+oddCycleGates g = Set.fromList $ map (Base . mkCycle) $ oddCycles g
 
 oddCycles :: Ord a => Graph a -> [[a]]
 oddCycles g = Map.elems
@@ -28,12 +34,12 @@ nubOrd = Set.toList . Set.fromList
 
 go :: (Ord a) => Graph a -> a -> a -> [a] -> Set a -> Int -> Map a (Set a) -> [[a]]
 go g s t path seen len n = if
-    (s `Set.member` (n Map.! t)) && (len >= 3) && (odd len)
-    then (path:rest)
+    (s `Set.member` (n Map.! t)) && len >= 3 && odd len
+    then path:rest
     else rest where
       rest = [ x |
         v <- Set.toList (n Map.! t),
-        not $ v `Set.member` seen,
+        v `Set.notMember` seen,
         x <- go g s v (v:path) (Set.insert v seen) (len + 1) n
         ]
 
@@ -71,13 +77,19 @@ data BWGraph a = BWGraph {
     bwEdges :: Set (a, a)
     } deriving (Eq, Ord)
 
+data Gate a = Base a | Rec (Set a) a deriving (Eq, Ord, Show)
+
+out :: Gate a -> a
+out (Base x)  = x
+out (Rec _ x) = x
+
 repr :: (Show a) => (a, BW) -> String
 repr (x, White) = show x ++ "w"
 repr (x, Black) = show x ++ "b"
 
 instance (Show a, Ord a) => (Show (BWGraph a)) where
     show g = show (
-        unwords $ map repr $ sort $ Map.toList $ (bwVertices g),
+        unwords $ map repr $ sort $ Map.toList (bwVertices g),
         show (bwEdges g)
         )
 
@@ -88,9 +100,9 @@ wVertices bwGraph = Map.keysSet
 
 hasEdge :: (Ord a) => Graph a -> a -> a -> Bool
 hasEdge g u v = let (u', v') = (min u v, max u v) in
-    (u', v') `Set.member` (edges g)
+    (u', v') `Set.member` edges g
 
-mkCBS :: (Ord a) => (Set a) -> (Set a) -> Set (a, a)
+mkCBS :: (Ord a) => Set a -> Set a -> Set (a, a)
 mkCBS us vs = Set.fromList $ [(u', v') |
     u <- Set.toList us,
     v <- Set.toList vs,
@@ -98,20 +110,20 @@ mkCBS us vs = Set.fromList $ [(u', v') |
     ]
 
 isCBS :: (Ord a) => Graph a -> Set a -> Set a -> Bool
-isCBS g b1 b2 = and [hasEdge g u v | u <- (Set.toList b1), v <- (Set.toList b2)]
+isCBS g b1 b2 = and [hasEdge g u v | u <- Set.toList b1, v <- Set.toList b2]
 
 mkCycle :: (Ord a) => [a] -> BWGraph a
 mkCycle vs = white
     $ fromEdgeList
-    $ (head vs, last vs):(zip vs (tail vs))
+    $ (head vs, last vs):zip vs (tail vs)
 
 white :: (Ord a) => Graph a -> BWGraph a
 white g = BWGraph {bwVertices=Map.fromSet (const White) (vertices g), bwEdges=edges g}
 
 merge :: (Ord a) => (BWGraph a, Set a) -> (BWGraph a, Set a) -> BWGraph a
 merge (b1, i1) (b2, i2) = BWGraph {
-    bwVertices = Map.mapWithKey (\v bw -> if v `Set.member` i1 || v `Set.member` i2 then Black else bw) $ (bwVertices b1) `Map.union` (bwVertices b2),
-    bwEdges = (bwEdges b1) `Set.union` (bwEdges b2) `Set.union` (mkCBS i1 i2)
+    bwVertices = Map.mapWithKey (\v bw -> if v `Set.member` (i1 \/ i2) then Black else bw) (Map.union (bwVertices b1) (bwVertices b2)),
+    bwEdges = bwEdges b1 \/ bwEdges b2 \/ mkCBS i1 i2
     }
 
 e :: (Ord a) => Int -> Graph a -> [BWGraph a]
@@ -124,6 +136,7 @@ e n graph = nubOrd $ [ x |
 
 xi :: (Ord a) => Graph a -> BWGraph a -> BWGraph a -> [BWGraph a]
 xi g b1 b2 = [merge (b1, i1) (b2, i2) |
+    b1 /= b2,
     (o1, i1) <- partitions $ wVertices b1,
     not $ null i1,
     (o2, i2) <- partitions $ wVertices b2,
@@ -134,50 +147,62 @@ xi g b1 b2 = [merge (b1, i1) (b2, i2) |
     isCBS g i1 i2
     ]
 
-calc :: (Ord a) => Graph a -> [BWGraph a] -> [BWGraph a] -> [BWGraph a]
-calc graph known new = if result == []
-    then nubOrd (known ++ new)
-    else nubOrd $ calc graph (nubOrd (known ++ new)) result
+calc :: (Ord a) => Graph a -> Set (Gate (BWGraph a)) -> Set (Gate (BWGraph a)) -> Set (Gate (BWGraph a))
+calc graph known new = if Set.null result
+    then known \/ new
+    else trace (show (Set.size result)) $ calc graph (known \/ new) result
     where
-      result = nubOrd $ [ x |
-        b1 <- known,
-        b2 <- new,
+      result = Set.fromList $ [ Rec (Set.fromList [b1, b2]) x |
+        b1 <- map out $ Set.toList known,
+        b2 <- map out $ Set.toList new,
         x <- xi graph b1 b2
         ]
 
-calcAll :: (Ord a) => Graph a -> [BWGraph a]
+calcAll :: (Ord a) => Graph a -> Set (Gate (BWGraph a))
 calcAll graph = calc graph oc oc where
-    oc = map mkCycle $ oddCycles graph
+    oc = oddCycleGates graph
 
 toGraphviz :: BWGraph Integer -> String
 toGraphviz g = "graph G {\n" ++
-    "    node [shape=circle, style=filled, label=\"\"];\n" ++
-    
-    unlines (map (\(v, bw) -> "    " ++ show v ++ " [fillcolor=" ++ (if bw == White then "white" else "black") ++ "];") (Map.toList (bwVertices g))) ++
-    
+    "    node [shape=circle, style=filled];\n" ++
+
+    unlines (map (\(v, bw) -> "    " ++ show v ++ " [fillcolor=" ++ (if bw == White then "white" else "black") ++ " fontcolor=" ++ (if bw == Black then "white" else "black") ++ "];") (Map.toList (bwVertices g))) ++
+
     unlines (map (\(u, v) -> "    " ++ show u ++ " -- " ++ show v ++ ";") (Set.toList (bwEdges g))) ++
-      
+
 
     "}"
 
+{-
 maxBy :: (Ord b) => (a -> b) -> [a] -> a
 maxBy f [x] = x
-maxBy f (x:xs) = let y = (maxBy f xs) in
+maxBy f (x:xs) = let y = maxBy f xs in
     if f x >= f y then x else y
-
-main = writeAll tripleBowtie
+-}
 
 writeAll :: Graph Integer -> IO ()
 writeAll g = do
-    let bwGraphs = calcAll g
     mapM_
         (\(b, i) -> writeFile
-            ("bwgraph-" ++ (show i) ++ "-images.dot")
+            ("bwgraph-" ++ i ++ "-images.dot")
             (toGraphviz b))
-        (zip bwGraphs [0..])
+        (Map.toList names)
 
-pick :: Graph Integer -> IO ()
-pick g = putStrLn $ toGraphviz $ maxBy (\g -> Map.size $ Map.filter (== Black) $ bwVertices g) $ calcAll g
+    writeFile "final.dot" (toDigraph names gates)
+      where
+        names = Map.fromList
+            $ (\x -> zip x (map show [0..]))
+            $ map out
+            $ Set.toList gates
+
+        bretts = Map.fromList $ (\x -> zip x (map show [0..])) $ Set.toList gates
+
+        gates = calcAll g
+
+
+
+-- pick :: Graph Integer -> IO ()
+-- pick g = putStrLn $ toGraphviz $ maxBy (\g -> Map.size $ Map.filter (== Black) $ bwVertices g) $ calcAll g
 
 brett = fromEdgeList [
     (0, 1),
@@ -203,8 +228,7 @@ tripleBowtie = fromEdgeList [
     (6, 8),
     (7, 8)
     ]
-    
-    
+
 tripleBowtieAndExtra = fromEdgeList [
     (0, 1),
     (0, 2),
@@ -224,8 +248,32 @@ tripleBowtieAndExtra = fromEdgeList [
     (11, 9)
     ]
 
-poop = fromWalks [
-    [0, 1, 2, 3, 4, 5, 6, 7, 8, 6, 5, 3, 2, 0]
-    ]
+boxedDiamond = fromWalks [
+    [0, 1, 2, 3, 4, 5, 0],
+    [0, 2, 3, 1]]
+
+toDigraph :: (Ord a) => Map a String -> Set (Gate a) -> String
+toDigraph graphs gates =
+    "digraph G {\n" ++
+    "    overlap=false;\n" ++
+    "    node [shape=none, label=\"\"];\n" ++
+
+    unlines (map (\(_, name) -> "    " ++ name ++ " [image=\"bwgraph-" ++ name ++ "-images.dot.png\"];" ) $ Map.toList graphs) ++
+
+    "    node [shape=circle, label=\"Ξ\"];\n" ++
+
+    unlines (zipWith (\g i -> digraphString g i (graphs Map.!)) (Set.toList gates) [0..]) ++
+
+    "}"
+
+digraphString :: Gate a -> Int -> (a -> String) -> String
+digraphString (Base _) _  _ = ""
+-- with intermediate xi gate
+digraphString (Rec ins out) index name = "    {" ++ intercalate ", " (map name (Set.toList ins)) ++ "} -> xi" ++ show index ++ " -> " ++ name out ++ ";"
+-- without
+-- digraphString (Rec ins out) index name = "    {" ++ intercalate ", " (map name (Set.toList ins)) ++ "} -> " ++ name out ++ ";"
 
 
+
+main :: IO ()
+main = writeAll boxedDiamond
